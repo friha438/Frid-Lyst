6e647b3bf81423f3481278e2c73f8b6228b378dd
Loual201 <louise.almer97@gmail.com>
2020-04-03 09:22:29 +0200

    test med shader graph och script

---
+++ Unity eld/RealFire/Assets/changeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeColor : MonoBehaviour
+{
+
+    public Material mat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mat.SetColor("_mainColor", new Color(0f, 10f, 0f, 10f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
