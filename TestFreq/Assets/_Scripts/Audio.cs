2969799fa9b43c7ce3a6a5c70720b75e42d252da
MalinRahm <malinrahm97@gmail.com>
2020-04-07 15:46:50 +0200

    Förinspelat ljud beroende på frekvenser

---
+++ TestFreq/Assets/_Scripts/Audio.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class Audio : MonoBehaviour
+{
+    AudioSource _audioSource;
+    public  static float[] _samples = new float[512];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetSpectrumAudioSource();
+    }
+
+    void GetSpectrumAudioSource()
+    {
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+
+}
+
---
+++ TestFreq/Assets/_Scripts/Instantiate515Cubes.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Instantiate515Cubes : MonoBehaviour
+{
+    public GameObject _sampleCubePrefab;
+    GameObject[] _sampleCube = new GameObject[512];
+    public float _maxScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            GameObject _instanceSampleCube = (GameObject)Instantiate(_sampleCubePrefab);
+            _instanceSampleCube.transform.position = this.transform.position;
+            _instanceSampleCube.transform.parent = this.transform;
+            _instanceSampleCube.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -0.703125f * i, 0);
+            _instanceSampleCube.transform.position = Vector3.forward * 100;
+            _sampleCube[i] = _instanceSampleCube;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            if(_sampleCube != null)
+            {
+                _sampleCube[i].transform.localScale = new Vector3(10, (Audio._samples[i] * _maxScale) + 2, 10);
+            }
+        }    
+    }
+}
