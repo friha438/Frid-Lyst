d406a4a7878bb5120880f0705c8e5a25a6ce5152
Alice <alika734@student.liu.se>
2020-04-30 14:23:55 +0200

    play och stop

---
+++ Ljud/Louise2_V3/Assets/Pause.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    changeColor play_fire;
+    public GameObject fire;
+    public bool play = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (play == true)
+        {
+            Debug.Log("hej");
+        }
+    }
+}
